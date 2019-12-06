2282e19e688c00693a829ef03fa5fc4074286dd0
FatinNabilah1 <40049342+FatinNabilah1@users.noreply.github.com>
2019-12-06 17:29:31 +0800

    Update ui.R

--- shiny/ui.R
+++ shiny/ui.R
@@ -10,13 +10,12 @@ shinyUI(fluidPage(
         # Application title
         titlePanel("Climate Change in Major Country"),
         navbarPage("", id="nav",
-           tabPanel("Interactive map",
+           tabPanel("Interactive Chart 1",
                 # Sidebar with controls to select city, month and type of plot
                 sidebarLayout(
         
                         sidebarPanel(
                                 helpText("Select one or more cities:"),
-                                
                                 uiOutput("CitySelector"),
                                 
                                 helpText("Select one or more months:"),
@@ -39,10 +38,30 @@ shinyUI(fluidPage(
                         mainPanel(
         
                                 textOutput("overview"),
-                                plotOutput("RegPlot")
+                                plotOutput("RegPlotCities")
                                 )
                         )
            ),
+           tabPanel("Interactive Chart 2",
+                    # Sidebar with controls to select City and year
+                    sidebarLayout(
+                            
+                            sidebarPanel(
+                                    helpText("Select one or more country:"),
+                                    uiOutput("CitysSelector"),
+                                    
+                                    
+                                    helpText("Select years:"),
+                                    uiOutput("yearSelector"),
+                            ),
+                            
+                            #Main Panel contains the plot/s
+                            mainPanel(
+                                    
+                                    textOutput("overview1"),
+                                    plotOutput("RegPlotCountry")
+                            )
+                    )),
            tabPanel("Data explorer",
                     basicPage(
                             DT::dataTableOutput("climatetable")
