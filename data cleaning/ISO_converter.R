7ee8027c04d81a61961ff26bfbc5f8b4e2d6a4fd
FatinNabilah1 <40049342+FatinNabilah1@users.noreply.github.com>
2019-12-07 17:30:39 +0800

    Create ISO_converter.R

---
+++ data cleaning/ISO_converter.R
@@ -0,0 +1,61 @@
+#import library
+library(magrittr)
+library(rvest)
+
+#retrive the ISO code
+url <- "https://www.nationsonline.org/oneworld/country_code_list.htm"
+iso_codes <- url %>%
+  read_html() %>%
+  html_nodes(xpath = '//*[@id="CountryCode"]') %>%
+  html_table()
+iso_codes <- iso_codes[[1]][, -1]
+iso_codes <- iso_codes[!apply(iso_codes, 1, function(x){all(x == x[1])}), ]
+names(iso_codes) <- c("Country", "ISO2", "ISO3", "UN")
+
+
+#################################################################################################################
+
+#import library
+library(maps)
+library(ggplot2)
+
+world_data <- ggplot2::map_data('world')
+world_data <- fortify(world_data)
+head(world_data)
+
+#change the country name in the climate data country to new name
+climate_data <- read.csv("modified_file1.csv", header = TRUE, stringsAsFactors = FALSE)
+old_names <- c("French Southern and Antarctic Lands", "Antigua", "Barbuda", "Saint Barthelemy", "Brunei", "CÃ´te D'Ivoire",
+               "Congo (Democratic Republic Of The)", "Republic of Congo", "Falkland Islands", "Micronesia", "UK", 
+               "Heard Island", "Cocos Islands", "Iran", "Nevis", "Saint Kitts", "South Korea", "Laos", "Saint Martin",
+               "Macedonia", "Pitcairn Islands", "North Korea", "Palestine", "Russia", "South Sandwich Islands",
+               "South Georgia", "Syria", "Trinidad", "Tobago", "Taiwan", "Tanzania", "USA", "Vatican", "Grenadines",
+               "Saint Vincent", "Venezuela", "Vietnam", "Wallis and Fortuna", "United States", "Burma")
+
+new_names <- c("French Southern Territories", rep("Antigua and Barbuda", 2), "Saint-Barthélemy",
+               "Brunei Darussalam", "Côte d'Ivoire", "Congo, (Kinshasa)", "Congo (Brazzaville)", 
+               "Falkland Islands (Malvinas)", "Micronesia, Federated States of", "United Kingdom",
+               "Heard and Mcdonald Islands", "Cocos (Keeling) Islands", "Iran, Islamic Republic of",
+               rep("Saint Kitts and Nevis", 2), "Korea (South)", "Lao PDR", "Saint-Martin (French part)",
+               "Macedonia, Republic of", "Pitcairn", "Korea (North)", "Palestinian Territory", "Russian Federation",
+               rep("South Georgia and the South Sandwich Islands", 2), 
+               "Syrian Arab Republic (Syria)", rep("Trinidad and Tobago", 2), "Taiwan, Republic of China",
+               "Tanzania, United Republic of", "United States of America", "Holy See (Vatican City State)",
+               rep("Saint Vincent and Grenadines", 2), "Venezuela (Bolivarian Republic)", "Viet Nam", "Wallis and Futuna Islands",
+               "United States of America", "Myanmar")
+
+for (i in 1:length(old_names)){
+  climate_data$Country[climate_data$Country == old_names[i]] <- new_names[i]
+}
+
+for (i in 1:length(old_names)){
+  
+  world_data$region[world_data$region == old_names[i]] <- new_names[i]
+  
+}
+#create new column in climate data file, and match the ISO code with the new name of country
+climate_data['ISO3'] <- iso_codes$ISO3[match(climate_data$Country, iso_codes$Country)]
+climate_data['ISO3'] <- iso_codes$ISO3[match(climate_data$Country, iso_codes$Country)]
+world_data["ISO3"] <- iso_codes$ISO3[match(world_data$region, iso_codes$Country)]
+
+write.csv(climate_data, "modified_file1.csv" ,row.names=FALSE)
